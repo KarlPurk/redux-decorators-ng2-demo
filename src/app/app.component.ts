bb1b447fd7d14e2907deb5c4b043b735efeb73a5
Karl Purkardt <karl.purkardt@nucleuscentral.com>
2016-07-12 13:08:59 +0100

    feat: Add basic redux decorator example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { Slice, Reducer, Store } from 'redux-decorators';
+
+@Slice('count', 0)
+@Reducer('add', 'remove')
+class ActionReducers {
+    add(count) { return count + 1}
+    remove(count) { return count - 1}
+}
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  template: `
+    <h1>Redux Decorators - Angular 2 Demo</h1>
+    <div>Count: {{count}}</div>
+    <hr />
+    <button (click)="dispatch('add')">Add</button>
+    <button (click)="dispatch('remove')">Remove</button>
+`
 })
+@Store('count')
 export class AppComponent {
   title = 'app works!';
 }
